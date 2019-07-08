0a3291e6cc5c56f6405299ad5ba520f65f729987
anusha <anushakarkera17@gmail.com>
2019-07-08 10:07:46 +0530

    updated

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent implements OnInit {
-
+servicesContent=[
+  {
+    img:"../../../assets/images/icons/rain.svg",
+    header:"We’ll Watch the Weather",
+    body:"Download our app and we’ll send you a notice if it’s about to rain in the next 20 minutes around your current location. A good rain can be refreshing, but sometimes you just want to stay dry."
+  },
+  {
+    img:"../../../assets/images/icons/wifi.svg",
+    header:"Wi-Fi Waypoints",
+    body:"We only send you on trips to places we can personally vouch for as being amazing. Which means we’ve mapped out where local wi-fi spots are and marked them in our app’s map view."
+  },
+  {
+    img:"../../../assets/images/icons/globe.svg",
+    header:"Global Guides",
+    body:"We’ve scoured the entire planet for the best retreats and beautiful vistas. If there’s a corner of the world you want to escape to we know the most scenic and inspiring locations."
+  },
+  {
+    img:"../../../assets/images/icons/fire.svg",
+    header:"Survival Kit",
+    body:"Everytime you book an escape with us we send you a survival kit with the finest materials. The kit will allow you to setup a tent, start a fire, scratch your own back and lower your taxes."
+  }
+]
   constructor() { }
 
   ngOnInit() {
--- src/app/components/testimonials/testimonials.component.ts
+++ src/app/components/testimonials/testimonials.component.ts
@@ -6,7 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./testimonials.component.css']
 })
 export class TestimonialsComponent implements OnInit {
-
+cards=[
+  {
+    img:"../../../assets/images/testimonial-jane.jpg",
+    name:"Jane Doe",
+    text:"9 Time Escaper",
+    content:"“Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”"
+  },
+  {
+    img:"../../../assets/images/testimonial-john.jpg",
+    name:"John Smith",
+    text:"4 Time Escaper",
+    content:"“Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur.”"
+  },
+  {
+    img:"../../../assets/images/testimonial-cat.jpg",
+    name:"Cat McKitty",
+    text:"6 Time Escaper",
+    content:"“Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.”"
+  }
+]
   constructor() { }
 
   ngOnInit() {
